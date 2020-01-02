502fc7296fc27b6257ebd30e39cf27c064fb3817
Chadwick Bidwell <cbidwell@proctoru.com>
2020-01-02 11:28:40 -0800

    adding test method

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,4 +2,11 @@ class Url < ActiveRecord::Base
 	extend FriendlyId
 	friendly_id :shortened
 	attr_accessible :original, :shortened
+
+
+	def test_method
+		Rails.logger.info "hi"
+		// does nothing
+	end
+	
 end
