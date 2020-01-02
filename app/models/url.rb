31c51d1edb64c190b5b57e2bb82ac7649232b830
Chadwick Bidwell <cbidwell@proctoru.com>
2020-01-02 11:30:47 -0800

    added stupid thing method

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,4 +2,10 @@ class Url < ActiveRecord::Base
 	extend FriendlyId
 	friendly_id :shortened
 	attr_accessible :original, :shortened
+
+	def stupid_thing
+		foo = 'hi'
+		Rails.logger.info "--foo: #{foo}"
+	end
+	
 end
