c1f43fd7e44734e0be2e9f3f7a06dd66814ce46f
Chadwick Bidwell <cbidwell@proctoru.com>
2020-01-02 11:32:55 -0800

    adding print_something method

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,4 +2,9 @@ class Url < ActiveRecord::Base
 	extend FriendlyId
 	friendly_id :shortened
 	attr_accessible :original, :shortened
+
+	def print_something
+		something = 'something'
+		Rails.logger.info "---something: #{something}"
+	end
 end
